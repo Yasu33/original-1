4dfb89bf1ef73cb7b4298d5aa9405454da196d92
Yuria kimura <yuriancoco@gmail.com>
2020-03-01 11:13:28 +0900

    save data 2020/03/01 -1

---
+++ original/RoguinViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RoguinViewController.swift
+//  original
+//
+//  Created by Kimura Yuria on 2020/03/01.
+//  Copyright © 2020 Kimura Yuria. All rights reserved.
+//
+
+import UIKit
+
+class RoguinViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
